61e0d1826210e2e1a0f7efa10b5bdda77d9f2c98
Mike M <mikester70387@gmail.com>
2025-03-04 20:58:29 -0800

    Added Plausible custom domain proxy prop to next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import { NextConfig } from 'next';
 import { withPlausibleProxy } from 'next-plausible';
+import { NextPlausiblePublicProxyOptions } from 'next-plausible/dist/lib/common';
 
 const nextConfig: NextConfig = {
 	images: {
@@ -15,4 +16,8 @@ const nextConfig: NextConfig = {
 	}
 };
 
-export default withPlausibleProxy()(nextConfig);
+const plausibleConfig: NextPlausiblePublicProxyOptions = {
+	customDomain: process.env.NEXT_PUBLIC_ANALYTICS_HOST
+}
+
+export default withPlausibleProxy(plausibleConfig)(nextConfig);
